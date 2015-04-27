40ae1bbb0da99e35aa5cc6e1dff70c770ff0921d
Kevin Isom <kevin.isom@gmail.com>
2015-04-28 06:13:47 +1200

    first steps

--- Episode 1/index.js
+++ Episode 1/index.js
@@ -1,5 +1,26 @@
 // Default Parameters
 
+function doSomething(obj, otherThing = 1) {
+
+}
+doSomething('my object');
+
+doSomething('my obj', 25);
 // Rest Parameters
 
+function add(first, ...numbers) {
+    let result = first;
+
+    for (var i = 0; i < numbers.length; i++) {
+        result = result + numbers[i];
+    }
+
+    return result;
+}
 // Spread Operator
+
+let numbers = [54, 32, 45, 12, 36, 89];
+
+let min = Math.min(...numbers);
+//old way
+var oldMin = Math.min.apply(Math, numbers);
